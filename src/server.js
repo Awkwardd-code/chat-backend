aab80b57876b076b091d7b78dccf4cb79da2996a
Awkwardd-code <somethinn999awkwardd@gmail.com>
2025-10-01 16:24:22 +0600

    small change

--- src/server.js
+++ src/server.js
@@ -30,8 +30,11 @@ app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+app.get("/", (req, res) => {
+    res.send("API is running...");
+});
 
 app.listen(PORT, () => {
     console.log("Server running on port: " + PORT);
     connectDB();
-  });
+});
